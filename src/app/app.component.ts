b803dc3c5dc296aacad5dc15b74943db5d441bba
Sebastian <jsruiz7411@misena.edu.co>
2021-02-16 15:44:59 -0500

    Piano Terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'piano';
+
+aplicarSonido(numero:number){
+
+  const audio = new Audio();
+
+  audio.src="../assets/sonidos/piano"+numero+".mp3";
+
+  audio.load();
+
+  audio.play();
+
+
+}
 }
